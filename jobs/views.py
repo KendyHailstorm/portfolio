3bfb90dd52c7b7749ba369a8f158ad53e1349df5
Black Pearl <falcon@localhost.localdomain>
2018-07-27 22:58:00 +0300

    successfully added the jobs blogs with images

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Job
 
 # Creating views to be viewed at home.
 
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects
+    return render(request, 'jobs/home.html',{'jobs':jobs})
